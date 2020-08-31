a62a10cee7693298a31afc92909bcd819db5f868
rajnish <rps.rajnish@gmail.com>
2020-08-31 21:13:17 +0530

    31-08-20

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\ProductPurchased;
+use App\Listeners\Award;
+use App\Listeners\SendCoupon;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,7 +20,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
+        ]
     ];
 
     /**
@@ -31,4 +34,11 @@ class EventServiceProvider extends ServiceProvider
 
         //
     }
+
+    public function shouldDiscoverEvents()
+    {
+        return true;
+    }
+
+
 }
